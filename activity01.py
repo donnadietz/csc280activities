c18c1641f31e4668a71799804ef7c799cf44d9e7
Donna Dietz <donna.dietz@gmail.com>
2019-08-20 12:11:31 -0400

    Activity 1

---
+++ activity01.py
@@ -0,0 +1,86 @@
+'''
+CSC 280
+Python3
+Activity #1:
+
+Open Python 3
+
+Type the following lines into the interpreter and copy whatever Python3 echoes back to you. Be careful to count spaces accurately for multiline entries.
+
+You should make a habit of creating your own examples, especially if you do not immediately understand what the computer has echoed.  For example, 100//7 may surprise you.  Try other integers in place of the 100 and the 7 to see what is going on.  If you do not understand after a few examples, ask the students nearest to you.  If you still do not understand, be sure to ask for help!
+'''
+5+5
+
+4*4
+
+100^2
+
+7**2
+
+8-4
+
+-1/10**99
+
+-1/10**9999
+
+100/7
+
+100//7
+
+100 % 7
+
+myName="George"
+
+for letter in myName:
+    print(letter)
+
+
+L=[0,1,2,3,4,5]
+for item in L:
+   print(item)
+
+for i in range(5):
+   print(i)
+
+type(range(3))
+
+type(5.5)
+
+type(7)
+
+type("h")
+
+type("hi")
+
+type([1,2,3])
+
+type( (3,3) )
+
+type(None)
+type(True)
+"Spam " "eggs"
+print("hi"+" there")
+print("hi" " there")
+print("hi", "there")
+"hi"*5
+"hi"+5
+"hi"+str(5)
+[1,2,3]*4
+"na"*16+"Batman"
+x=7
+x+5
+x**3
+y=4
+sqrt(y)
+y**0.5
+x,y = y,x
+x
+y
+"All "+ str(x)+ " one and one "+ str(x)+ " all."
+#This is a comment. THE COMPUTER CAN'T HEAR WHAT YOU SAY HERE!!!
+
+'''Learning objectives for this activity:
+For numerical types: adding, multiplying, raising to powers, subtracting (We will talk about what ^ means later.)
+Integer division: // and % You can decompose strings into character components naturally in Python3. There are various "types" such as lists, tuples, range, integers, float, negative zero (just for fun!). Use of the "type" command for telling what type you have. Some basic fun and/or useful string manipulation tricks. Casting integers as strings for use in output. Single line comments.
+'''
+
